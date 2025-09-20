c38c13d68e008e43728ab7b1ea991043293c90fa
ashsihBidPoint <ashish@bidpoint.in>
2025-09-20 05:31:17 +0530

    login signup frontend done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,17 @@ import messageRoutes from './routes/messageRoutes.js'
 import dotenv from 'dotenv'
 import { connectDb } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 
 dotenv.config()
 
 const app = express();
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use(express.json()) 
 const PORT = process.env.PORT || 5001
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import Navbar from "./component/Navbar";
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const {checkAuth, authuser, isCheckingAuth} = useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  }, [checkAuth])
+  console.log({authuser});
+  
+  if(!authuser && isCheckingAuth){
+    return (
+      <div className="h-screen w-full flex justify-center items-center">
+        <Loader className="animate-spin h-10 w-10 text-blue-500" />
+      </div>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={authuser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authuser ? <SignupPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authuser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={ <SettingPage />} />
+        <Route path="/profile" element={authuser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
 
-export default App
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
